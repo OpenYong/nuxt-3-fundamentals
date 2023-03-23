fa1cd7ae3ddfc7740e8d80529f3390c7babee526
OpenYong <fkszm3@gmail.com>
2023-03-24 08:45:29 +0900

    Call the useHead Composable for Good SEO

--- app.vue
+++ app.vue
@@ -1,3 +1,17 @@
+<script setup>
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - Yongs 영화 앱` : "Yongs 영화 앱";
+  },
+  meta: [
+    {
+      name: "description",
+      content: "Nuxt.js is a web application framework",
+    },
+  ],
+});
+</script>
+
 <template>
   <div>
     <nav>
--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -3,10 +3,11 @@ const route = useRoute();
 const { data, error } = await useFetch(
   `http://www.omdbapi.com/?apikey=74d733cd&i=${route.params.id}`,
   {
-    pick: ["Title", "Plot", "Error"],
+    pick: ["Title", "Plot", "Poster", "Error"],
     key: `/movies/${route.params.id}`,
   }
 );
+console.log(data);
 if (error.value) {
   showError({
     statusCode: 500,
@@ -20,6 +21,19 @@ if (data.value.Error === "Incorrect IMDb ID.") {
     statusMessage: "영화를 찾을 수 없습니다.",
   });
 }
+
+useHead({
+  title: data.value.Title,
+  meta: [
+    {
+      name: "description",
+      content: data.value.Plot,
+    },
+    { property: "og:description", content: data.value.Plot },
+    { property: "og:image", content: data.value.Poster },
+    { name: "twitter:card", content: `summary_large_image` },
+  ],
+});
 
 // const { data } = await useAsyncData(
 //   `/movies/${route.params.id}`,
