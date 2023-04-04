1672e2c247c066872473035a4249d2422d7a8901
OpenYong <fkszm3@gmail.com>
2023-04-04 10:13:16 +0900

    Global State Management with useState

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,16 @@
+<script setup>
+const isLoggedIn = useIsLoggedIn();
+</script>
+
 <template>
   <nav style="display: flex; gap: 1rem">
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/movies">Movies</NuxtLink>
     <NuxtLink to="https://nuxt.com/modules">Nuxt Moduels</NuxtLink>
+    <NuxtLink v-if="!isLoggedIn" to="/login">로그인</NuxtLink>
+    <a href="#" v-if="isLoggedIn" @click.prevent="isLoggedIn = false"
+      >로그아웃</a
+    >
   </nav>
   <slot> </slot>
 </template>
--- pages/login.vue
+++ pages/login.vue
@@ -1,11 +1,17 @@
 <script setup>
+const isLoggedIn = useState("isLoggedIn", () => false);
 definePageMeta({
   layout: "plain",
 });
+
+function login() {
+  isLoggedIn.value = true;
+  useRouter().push("/");
+}
 </script>
 
 <template>
-  <form action="">
+  <form @submit.prevent="login">
     <h1>로그인</h1>
     <label for="name"
       >이름
@@ -15,5 +21,6 @@ definePageMeta({
       >비밀번호
       <input type="password" name="password" id="password" />
     </label>
+    <button>로그인</button>
   </form>
 </template>
